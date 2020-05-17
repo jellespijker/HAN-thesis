0cf39b952d970a460875f5da0669c58ec955bb1a
Jelle Spijker <spijker.jelle@gmail.com>
2020-05-17 10:53:25 +0200

    major rework

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export LANG="en_US.UTF8"
+rm -rf build/
+mkdir build
+time lualatex --draftmode -interaction=nonstopmode -file-line-error -shell-escape -recorder -output-directory="build" -jobname="report" "report.tex" | awk 'BEGIN{IGNORECASE = 1}/warning|!/,/^$/;'
+time biber --isbn-normalise --validate_datamodel build/report
+cd build
+time makeglossaries report
+cd ..
+time lualatex --draftmode -interaction=nonstopmode -file-line-error -shell-escape -recorder -output-directory="build" -jobname="report" "report.tex" | awk 'BEGIN{IGNORECASE = 1}/warning|!/,/^$/;'
+time lualatex -interaction=nonstopmode -file-line-error -shell-escape -recorder -output-directory="build" -jobname="report" "report.tex" | awk 'BEGIN{IGNORECASE = 1}/warning|!/,/^$/;'
+mv build/report.pdf .
+okular report.pdf &
