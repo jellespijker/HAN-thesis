30656905b42614c3b3a22ffb7dae463fbfe6f6cc
Jelle Spijker <spijker.jelle@gmail.com>
2020-05-31 19:55:17 +0200

    Simulation description and results discussed

--- resources/scripts/curapp.py
+++ resources/scripts/curapp.py
@@ -49,13 +49,6 @@ cy_2 = np.flip(cover_y[:35])
 cy = np.append(cy_1, cy_2)
 
 #%%
-# lbl1 = range(0, len(cx))
-# fi1, ax1 = plt.subplots()
-# ax1.plot(cx, cy)
-# for i, txt in enumerate(lbl1):
-#     ax1.annotate(txt, (cx[i], cy[i]))
-
-#%%
 
 points = [(x, y) for x, y in zip(cx, cy)]
 mut_points = [points[0]]
@@ -67,10 +60,6 @@ for i in range(len(points) - 1):
     
 cover_orig = LineString(points)
 cover = LineString(mut_points)
-#%%
-# plt.plot(*cover_orig.xy, label='orig')
-# plt.plot(*cover.xy, label='tw')
-# plt.show()
 
 #%%
 
@@ -86,7 +75,7 @@ cover_d = LineString(discretize(cover, 50000))
 cover_d_orig = LineString(discretize(cover_orig, 50000))
 
 #%%
-cover_s = skew(cover_d, xs=0.5, ys=-5, origin=points[0])
+cover_s = skew(cover_d, xs=0.5, ys=-7, origin=points[0])
 ocover_s = skew(cover_d_orig, xs=0.5, ys=0.5, origin=points[0])
 
 
@@ -94,21 +83,21 @@ ocover_s = skew(cover_d_orig, xs=0.5, ys=0.5, origin=points[0])
 rc = np.array(cover_s.coords[0]) - np.array(cover_s.coords[1])
 orc = np.array(cover_s.coords[0]) - np.array(cover_s.coords[1])
 
-ocover_sc = scale(ocover_s, 0.99, 0.99, origin=cover_s.coords[0])
-cover_sc = scale(cover_s, 0.96, 0.96 * np.max(rc)/np.min(rc), origin=cover_s.coords[0])
+ocover_sc = scale(ocover_s, 0.987, 0.987, origin=cover_s.coords[0])
+cover_sc = scale(cover_s, 0.97, 0.95 * np.max(rc)/np.min(rc), origin=cover_s.coords[0])
 
 #%%
 rg = np.random.default_rng(12345)
 points_diff = []
 opoints_diff = []
-for p in cover_sc.coords:
-    rx = p[0] + 1.1*rg.random()
-    ry = p[1] + 1.1*rg.random()
+for idx, p in enumerate(cover_sc.coords):
+    rx = p[0] + (2.6 + idx / 20000) * rg.random()
+    ry = p[1] + (2.6 + idx / 20000) * rg.random()
     points_diff.append(Point(rx, ry))
 
 for p in ocover_sc.coords:
-    rx = p[0] + 0.3 * rg.random()
-    ry = p[1] + 0.3 * rg.random()
+    rx = p[0] + 2 * rg.random()
+    ry = p[1] + 2 * rg.random()
     opoints_diff.append(Point(rx, ry))
 
 mutated_cover = LineString(points_diff)
@@ -116,13 +105,8 @@ omutated_cover = LineString(opoints_diff)
 #%%
 inset = [Point(d['x'], d['y']) for d in data if d['line type'] == 'Inset0Type']
 inset = inset[1:]
+inset.append(Point(123.879, -108.12))
 ip = Polygon(inset)
-
-plt.plot(*mutated_cover.xy)
-plt.plot(*omutated_cover.xy)
-plt.plot(*ip.exterior.xy)
-
-plt.show()
 
 #%%
 np.savetxt('coverage_mutated.dat', np.array(mutated_cover.coords), delimiter=',')
@@ -133,8 +117,105 @@ np.savetxt('coverage_optimal.dat', np.array(omutated_cover.coords), delimiter=',
 #%%
 nees_mutated = []
 nees_optimal = []
-for p, m, o in zip(cover_d.coords, mutated_cover.coords, omutated_cover.coords):
+for p, m, o in zip(cover_d_orig.coords, mutated_cover.coords, omutated_cover.coords):
     nees_mutated.append(mean_squared_error(p, m))
     nees_optimal.append(mean_squared_error(p, o))
 np.savetxt('NEES_mutated.dat', np.array(nees_mutated), delimiter=',')
 np.savetxt('NEES_optimal.dat', np.array(nees_optimal), delimiter=',')
+
+#%%
+
+import pandas as pd
+
+idx = []
+for i in range(int(len(nees_optimal)/50)):
+    for j in range(50):
+        idx.append(i)
+
+#%%
+df_mut = pd.DataFrame({'distance': idx, 'error': nees_optimal, 'sim': ['optimal'] * len(nees_optimal)})
+df_opt = pd.DataFrame({'distance': idx, 'error': nees_mutated, 'sim': ['normal'] * len(nees_optimal)})
+df = pd.concat([df_opt, df_mut])
+
+# sns.set()
+# sns.set(style="whitegrid")
+# sns.set_context("paper")
+# f, ax = plt.subplots(figsize=(15, 6.5))
+# sns.despine(f, left=True, bottom=True)
+# pal = sns.color_palette("Reds", n_colors=2)
+# 
+# sns.lineplot(x='distance', y='error', data=df, hue='sim', palette=pal, ax=ax)
+
+#%%
+pal = sns.color_palette("Reds", n_colors=2)
+
+pdf_orig = pd.DataFrame({'x': cover_d_orig.coords.xy[0], 'y': cover_d_orig.coords.xy[1], 'sim': ['analytic'] * len(
+    cover_d_orig.coords.xy[1])})
+
+#ox, oy, mx, my = [], [], [], []
+# ox, oy, mx, my = omutated_cover.coords.xy[0][::100], omutated_cover.coords.xy[1][::100], mutated_cover.coords.xy[0][::100], \
+#                  mutated_cover.coords.xy[1][::100]
+ox, oy, mx, my = omutated_cover.coords.xy[0][::20], omutated_cover.coords.xy[1][::20], mutated_cover.coords.xy[0][::20], \
+                 mutated_cover.coords.xy[1][::20]
+# ox, oy, mx, my = ocover_sc.coords.xy[0][::100], ocover_sc.coords.xy[1][::100], cover_sc.coords.xy[0][::100], \
+#                  cover_sc.coords.xy[1][::100]
+
+
+#%%
+from scipy import signal
+
+b, a = signal.butter(3, 0.05)
+zi = signal.lfilter_zi(b, a)
+z, _ = signal.lfilter(b, a, omutated_cover.coords.xy[0], zi=zi * omutated_cover.coords.xy[0][0])
+z2, _ = signal.lfilter(b, a, z, zi=zi * z[0])
+fox = signal.filtfilt(b, a, omutated_cover.coords.xy[0])
+foy = signal.filtfilt(b, a, omutated_cover.coords.xy[1])
+fmx = signal.filtfilt(b, a, mutated_cover.coords.xy[0])
+fmy = signal.filtfilt(b, a, mutated_cover.coords.xy[1])
+
+pdf_opt = pd.DataFrame({'x': fox, 'y': foy, 'sim': ['optimal'] * len(fox)})
+pdf_mut = pd.DataFrame({'x': fmx, 'y': fmy, 'sim': ['normal'] * len(fox)})
+pdf = pd.concat([pdf_orig, pdf_opt, pdf_mut])
+boundary = pd.DataFrame({'x': fmx, 'y': fmy, 'sim': ['normal'] * len(fox)})
+sns.set()
+sns.set(style="whitegrid")
+sns.set_context("paper")
+pal = sns.color_palette("Reds", n_colors=2)
+sns.lineplot(x='distance', y='error', data=df, hue='sim', palette=pal)
+
+sns.set()
+sns.set(style="whitegrid")
+sns.set_context("paper")
+pal = sns.color_palette("Reds", n_colors=1)
+f, ax = plt.subplots(figsize=(6.5, 6.5))
+sns.despine(f, left=True, bottom=True)
+pal = sns.color_palette("Reds", n_colors=1)
+ax.fill(*ip.exterior.xy, color=(1., 0., 0., 0.15))
+sns.lineplot(x='x', y='y', data=pdf_opt, hue='sim', sort=False, palette=pal, ax=ax)
+f, ax = plt.subplots(figsize=(6.5, 6.5))
+sns.despine(f, left=True, bottom=True)
+pal = sns.color_palette("Reds", n_colors=1)
+ax.fill(*ip.exterior.xy, color=(1., 0., 0., 0.15))
+sns.lineplot(x='x', y='y', data=pdf_mut, hue='sim', sort=False, palette=pal, ax=ax)
+f, ax = plt.subplots(figsize=(6.5, 6.5))
+sns.despine(f, left=True, bottom=True)
+pal = sns.color_palette("Reds", n_colors=1)
+ax.fill(*ip.exterior.xy, color=(1., 0., 0., 0.15))
+sns.lineplot(x='x', y='y', data=pdf_orig, hue='sim', sort=False, palette=pal, ax=ax)
+# g = sns.FacetGrid(pdf, col="sim", palette=[pal]*3)
+# g.map(sns.lineplot, 'x', 'y', size=2, linewidth=1, sort=False, color=pal[0])
+
+#%%
+# pal = sns.color_palette("Reds", n_colors=3)
+# g = sns.FacetGrid(pdf, col="sim", palette=pal)
+# g.map(sns.lineplot, 'x', 'y', size=2, linewidth=1, sort=False, palette=pal)
+
+#%%
+# sns.set()
+# sns.set_context("paper")
+# pal = sns.color_palette("Reds", n_colors=2)
+# fig, ax = plt.subplots()
+# sns.lineplot(x='x', y='y', data=pdf, sort=False, hue='sim', palette=pal, ax=ax)
+# 
+# #sns.scatterplot(x='x', y='y', data=pdf, hue='sim', ci='sd', palette=pal, ax=ax)
+# sns.lineplot(x='x', y='y', data=pdf_orig, sort=False, hue='sim', palette=[(0.25, 0.25, 0.25)], size=1, ax=ax)
