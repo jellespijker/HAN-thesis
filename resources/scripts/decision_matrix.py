633db57fd0d1840cf3e8ec5b9d48f4c31e10c6c2
Jelle Spijker <spijker.jelle@gmail.com>
2020-06-04 02:08:47 +0200

    Completed decision matrix

--- resources/scripts/decision_matrix.py
+++ resources/scripts/decision_matrix.py
@@ -1,9 +1,10 @@
 #%%
-from uncertain_panda import pandas as pd
 # import pandas as pd
+from uncertain_panda import pandas as pd
 from uncertainties import *
 
 strategies = [
+    'weight',
     # 'MCD',  # morse based cellular decomposition
     # 'BCD',  # boustrophedon based cellular decomposition
     'onBCD',  # on-line boustrophedon based cellular decomposition
@@ -21,181 +22,172 @@ strategies = [
 
 #%% add online criterium
 online      = [ # Is the algorithm suitable for online usage (Leave this out since I allready filter on it?)
+                3,
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
+               ufloat(5, 0),
+               ufloat(5, 0),
+               ufloat(5, 0),
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(5, 0.2),
+               ufloat(5, 0),
+               ufloat(5, 0),
+               # ufloat(5, 0.2),
+               ufloat(5, 0),
+               ufloat(5, 0),
+               ufloat(5, 0)]
+energy_eff  = [ # How energy efficient is the algorithm in, taking in to account accelerations, decelirations, turns, revisits, drag etc
+               1,
+               # ufloat(1, 0.2),
+               # ufloat(1, 0.2),
+               ufloat(4, 1),
+               ufloat(4, 1),
+               ufloat(4, 0.5),
+               # ufloat(1, 0.2),
+               # ufloat(1, 0.2),
+               ufloat(5, 1),
+               ufloat(1, 0.5),
+               # ufloat(5, 0.2),,
+               ufloat(3, 1),
+               ufloat(3, 1),
+               ufloat(2, 2)]
+time  = [ # how much time
+                2,
+               # ufloat(1, 0.2),
+               # ufloat(1, 0.2),
+               ufloat(5, 2),
+               ufloat(5, 2),
+               ufloat(5, 1),
+               # ufloat(1, 0.2),
+               # ufloat(1, 0.2),
+               ufloat(5, 2.5),
+               ufloat(1, 2),
+               # ufloat(5, 0.2),
+               ufloat(3, 2),
+               ufloat(3, 2),
+               ufloat(2, 1)]
+revisits    = [ # How many revisits on certain locations
+                3,
+               # ufloat(1, 0.2),
+               # ufloat(1, 0.2),
+               ufloat(1, 0.2),
+               ufloat(1, 0.2),
+               ufloat(1, 0.2),
+               # ufloat(1, 0.2),
+               # ufloat(1, 0.2),
+               ufloat(1, 0.2),
                ufloat(1, 0.2),
                # ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2)]
-energy_eff  = [ # How energy efficient is the algorithm in, taking in to account accelerations, decelirations, turns, revisits, drag etc
-               # ufloat(1, 0.2),
-               # ufloat(1, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               # ufloat(1, 0.2),
-               # ufloat(1, 0.2),
-               ufloat(5, 0.2),
-               ufloat(1, 0.4),
-               # ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(2, 0.1)]
-time  = [ # how much time
-               # ufloat(1, 0.2),
-               # ufloat(1, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               # ufloat(1, 0.2),
-               # ufloat(1, 0.2),
-               ufloat(5, 0.2),
-               ufloat(1, 0.4),
-               # ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(5, 0.2),
-               ufloat(2, 0.1)]
-revisits    = [ # How many revisits on certain locations
-               # ufloat(1, 0.2),
-               # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               # ufloat(1, 0.2),
-               # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               # ufloat(5, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(1, 0.2),
+               ufloat(1, 0.2),
+               ufloat(1, 0.2),
+               ufloat(1, 0.2),
                ufloat(3, 0.2)]
 complexity  = [ # Implementation complexity
+                3,
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(5, 1),
+               ufloat(5, 1),
+               ufloat(4, 0.5),
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(1, 1),
+               ufloat(1, 2),
                # ufloat(5, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(1, 1),
+               ufloat(1, 2),
                ufloat(2, 0.5)]
 adaptivity  = [ # Performance in a changing environment
+                1,
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(2, 1),
+               ufloat(3, 1),
+               ufloat(3, 1),
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(5, 0.5),
+               ufloat(5, 0.5),
                # ufloat(5, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(4, 0.2)]
+               ufloat(3, 2),
+               ufloat(3, 2),
+               ufloat(4, 1)]
 maintainability = [ # Bug fixes regression in between commits
+                2,
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(5, 2),
+               ufloat(5, 2),
+               ufloat(4, 1),
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(3, 1),
+               ufloat(3, 1),
                # ufloat(5, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(2, 0.5)]
+               ufloat(2, 1),
+               ufloat(2, 1),
+               ufloat(2, 2)]
 TRL          = [ # Technological Readiness Level (novel --> proven)
+                3,
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(5, 1),
+               ufloat(5, 1),
+               ufloat(5, 0.5),
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(2, 2),
+               ufloat(2, 2),
                # ufloat(5, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(1, 0.2)]
+               ufloat(3, 1),
+               ufloat(3, 1),
+               ufloat(1, 3)]
 integration  = [  # how wel it behaves with the rest of the crawler components and controllers
+                1,
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(4, 0.5),
+               ufloat(4, 0.5),
+               ufloat(4, 0.5),
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(2, 1),
+               ufloat(2, 1),
                # ufloat(5, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(3, 0.2)]
+               ufloat(4, 2),
+               ufloat(4, 2),
+               ufloat(4, 2)]
 rulesregulation  = [ # Is it allowed by law
+                1,
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(3, 2),
+               ufloat(3, 2),
+               ufloat(3, 2),
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(2, 3),
+               ufloat(2, 3),
                # ufloat(5, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(2, 0.5)]
+               ufloat(3, 2),
+               ufloat(3, 2),
+               ufloat(2, 2)]
 environment  = [ # Environmental impact (sound,marine life)
+                1,
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(3, 1),
+               ufloat(3, 1),
+               ufloat(3, 1),
                # ufloat(1, 0.2),
                # ufloat(1, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
+               ufloat(2, 1),
+               ufloat(2, 1),
                # ufloat(5, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(0, 0.2),
-               ufloat(2, 0.5)]
+               ufloat(3, 1),
+               ufloat(3, 1),
+               ufloat(2, 1)]
 #%%
 data = [
     pd.Series(data=dict(zip(strategies, online)), name='online'),
@@ -211,11 +203,17 @@ data = [
     pd.Series(data=dict(zip(strategies, environment)), name='environment')
 ]
 
+#%%
 pugh = pd.DataFrame(data, columns=strategies)
+df_1 = pugh[pugh['weight'] == 1][pugh.columns[1:]].apply(lambda x: x)
+df_2 = pugh[pugh['weight'] == 2][pugh.columns[1:]].apply(lambda x: x * 2)
+df_3 = pugh[pugh['weight'] == 3][pugh.columns[1:]].apply(lambda x: x * 3)
+pg = pd.concat([df_1, df_2, df_3])
+sigma = pg.sum()
 
 #%%
-c = [' X[2,c,m]'] * len(strategies)
-out = '{\n\\noindent\n\\taburowcolors[2] 2{RoyalWhite .. RoyalLightGrey}\n\\begin{longtabu}{X[3,r,m]' + ''.join(c) + '}\n\\rowcolor{RoyalRed} \\rowfont{\\sffamily\\bfseries\\color{RoyalWhite}}\n  \\color{RoyalWhite}'
+c = [' X[3,c,m]'] * (len(strategies) - 1)
+out = '{\n\\noindent\n\\taburowcolors[2] 2{RoyalWhite .. RoyalLightGrey}\n\\begin{longtabu}{X[7,r,m] X[1,cm]' + ''.join(c) + '}\n\\rowcolor{RoyalRed} \\rowfont{\\sffamily\\bfseries\\color{RoyalWhite}}\n  \\color{RoyalWhite}'
 for col in pugh.columns:
     out += ' & \\color{RoyalWhite} \\rotatebox[origin=l]{90}{'
     out += '{}'.format(col)
@@ -224,11 +222,17 @@ out += '\\\\\n'
 for name, row in pugh.iterrows():
     out += '  \\textbf{' + str(name) + '}'
     for i in row.values:
-        val = ' & \\({:.1fL}\\)'.format(i)
+        if isinstance(i, int):
+            val = ' & \\({:.1f}\\)'.format(i)
+        else:
+            val = ' & \\({:.1fL}\\)'.format(i)
         val = val.replace('.0', '')
         out += val
     out += '\\\\\n'
-out += '\\end{longtabu}\n}\n'
+out += '\\rowcolor{RoyalRed} \\rowfont{\\sffamily\\bfseries\\color{RoyalWhite}}\n  \\color{RoyalWhite} \\(\\Sigma\\) & '
+for _, col in sigma.items():
+    out += '&  \\color{{RoyalWhite}} {:.1fL}'.format(col)
+out += '\n\\end{longtabu}\n}\n'
 
 with open('../strategy_decision_matrix.tex', 'w') as f:
     f.write(out)
